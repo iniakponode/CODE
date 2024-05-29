b2c86070d3d408b51aedfc990728145ac3ba7931
Thompson <r02it21@abdn.ac.uk>
2024-05-29 15:59:02 +0100

    Updated test_student file

---
+++ test_students.py
@@ -0,0 +1,28 @@
+# a=3
+# a=4
+# if a<4:
+#     print("The output is less than 4")
+# elif a>4:
+#     print("The output is greater than 4")
+
+
+# print("No condition was met in the else blocks")
+
+# for each_number in range(10):
+#     print(each_number)
+    
+    
+for each_number in range(10):
+    print(each_number)
+    if each_number==3:
+        continue
+    elif each_number==5:
+        break 
+    elif each_number==1:
+        pass
+    
+    
+
+    
+
+    
